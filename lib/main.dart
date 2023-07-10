5f375fcbacf65a00755f8754d503bcd02f6e85e4
Plaifah <praypar5689@gmail.com>
2023-07-10 15:50:39 +0700

    first app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_week9/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home()
+      );
+  }
+}
+
